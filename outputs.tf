b7eab8ff22e269c17787c830e0bea3f1519c4d2e
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2025-08-01 15:17:02 +0100

    feat: extended outputs (local branch) (#17)
    * feat: add outputs for private endpoints and resource details
    
    * Applied recomendations from `./avm pr-check`
    
    * refactor: applied TFFR2
    
    * fix: update response export values to include specific properties for endpoints and network interfaces
    
    * chore: pre commit
    
    ---------
    
    Co-authored-by: Marcin Słowikowski <59068696+msl0@users.noreply.github.com>
    Co-authored-by: github-actions[bot] <tfmod442916@users.noreply.github.com>

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,16 @@
+output "endpoint" {
+  description = "The default hostname of the resource."
+  value       = azapi_resource.this.output.properties.endpoint
+}
+
 output "name" {
   description = "The name of the resource."
   value       = azapi_resource.this.name
+}
+
+output "private_endpoint_network_interface_ids" {
+  description = "A map of the private endpoints created to their network interface ids."
+  value       = { for k, v in azapi_resource.private_endpoints : k => v.output.properties.networkInterfaces[*].id }
 }
 
 output "private_endpoint_resource_ids" {
